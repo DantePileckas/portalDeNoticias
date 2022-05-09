7ae8806ab46ba2da40df6df3f53a7ea33ed6059a
DantePileckas <pileckas33@gmail.com>
2022-05-09 16:44:24 -0300

    consulta a la API de newApi

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React, {Component} from "react";
 import Header from "./componentes/Header"
 
-function App() {
+class App extends Component{
+  
+ /*  state = {
+    noticias: []
+  } */
+
+  componentDidMount(){
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = () => {
+    let url = 'https://newsapi.org/v2/top-headlines?country=ar&category=general&apiKey=9c8713da4065456184a1e5bbc953c940';
+  
+/*     console.log(url)
+ */
+    fetch(url)
+    .then(respuesta=>{
+      return respuesta.json
+    })
+    .then(noticias =>{
+/*       console.log(noticias.articles)
+ */      this.setState({
+       noticias : noticias.articles
+     })  
+    }) 
+
+  }
+
+  render(){ 
   return (
     <div className="App">
       <Header
@@ -11,5 +39,5 @@ function App() {
     </div>
   );
 }
-
+}
 export default App;
