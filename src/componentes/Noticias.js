cf767910d39b8a00fa1e0270b8a84831c812defc
DantePileckas <pileckas33@gmail.com>
2022-05-09 21:14:28 -0300

    Interfaz de Noticia. Mostrando los datos y dirección de la Noticia

--- src/componentes/Noticias.js
+++ src/componentes/Noticias.js
@@ -6,8 +6,15 @@ class Noticias extends Component{
     render(){
         return(
             <div className="row">
-                <Noticia
-                />
+                
+                {this.props.noticias.map(noticia=>(
+                    <Noticia
+                    key={noticia.url}
+                    noticia={noticia}
+                    />
+                ))
+                }    
+
             </div>
             )
     }
