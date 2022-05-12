22e08475f82a905cdf6daffe6ae68846dc88df0c
DantePileckas <pileckas33@gmail.com>
2022-05-12 15:33:33 -0300

    Resolución de problema: Formulario

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,10 +1,9 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+class Formulario extends Component {
 
-class Formulario extends Component{
- 
-    categoriaRef = React.createRef();
+     categoriaRef = React.createRef();
 
      cambiarCategoria = (e) => {
           e.preventDefault();
@@ -15,37 +14,35 @@ class Formulario extends Component{
           this.props.consultarNoticias(this.categoriaRef.current.value);
      }
 
-     render() { 	
-        return (  	
-             <div className="buscador row">	
-                  <div className="col s12 m8 offset-m2">	
-                       <form onSubmit={this.cambiarCategoria}>	
-                            <h2>Noticias por Categoria</h2>	
-                            <div className="input-field col s12 m8">	
-                                 <select ref={this.categoriaRef}>	
-                                      <option value="general" defaultValue>General</option>	
-                                      <option value="business">Negocios</option>	
-                                      <option value="entertainment">Entretenimiento</option>	
-                                      <option value="health">Salud</option>	
-                                      <option value="science">Ciencia</option>	
-                                      <option value="sports">Deportes</option>	
-                                      <option value="technology">Tecnología</option>	
-                                 </select>	
-                            </div>	
-                            <div className="input-field col s12 m4 enviar">	
-                                 <input type="submit" className="btn amber darken-2" value="Buscar" />	
-                            </div>	
-                       </form>	
-                  </div>	
-             </div>	
-        )	
-   }
+     render() { 
+          return (  
+               <div className="buscador row">
+                    <div className="col s12 m8 offset-m2">
+                         <form onSubmit={this.cambiarCategoria}>
+                              <h2>Noticias por Categoria</h2>
+                              <div className="input-field col s12 m8">
+                                   <select className='select' ref={this.categoriaRef}>
+                                        <option value="general" defaultValue>General</option>
+                                        <option value="business">Negocios</option>
+                                        <option value="entertainment">Entretenimiento</option>
+                                        <option value="health">Salud</option>
+                                        <option value="science">Ciencia</option>
+                                        <option value="sports">Deportes</option>
+                                        <option value="technology">Tecnología</option>
+                                   </select>
+                              </div>
+                              <div className="input-field col s12 m4 enviar">
+                                   <input type="submit" className="btn amber darken-2" value="Buscar" />
+                              </div>
+                         </form>
+                    </div>
+               </div>
+          )
+     }
 }
 
 Formulario.propTypes = {
-    consultarNoticias : PropTypes.func.isRequired
+     consultarNoticias : PropTypes.func.isRequired
 }
-
-
-
+ 
 export default Formulario;
